3cbf9f4f64e22a9a3ea873cd6081dc4b7e551e7b
Marcos Maximo <marcosvinmaximo@gmail.com>
2022-07-27 18:44:40 -0300

    initial github transfer

--- script.js
+++ script.js
@@ -2,11 +2,21 @@ const btns = document.querySelectorAll("button");
 const div = document.querySelector(".modal-1");
 const buttonActive = document.querySelector('.button-modal-1')
 
+const toggleAriaHidden = () => {
+  if(div.getAttribute('aria-hidden') === 'true'){
+    div.setAttribute('aria-hidden', 'false')
+  } else {
+    div.setAttribute('aria-hidden', 'true')
+  }
+}
+
 const toggleModal = (e) => {
   for (const button of btns) {
     if (button === e.target) div.classList.toggle("hidden");
   }
   (e.target !== div) ? console.log() : div.classList.toggle("hidden");
+
+  toggleAriaHidden()
 };
 
 const elementsEvent = [buttonActive, div];
